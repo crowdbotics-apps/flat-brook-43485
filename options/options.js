f8f591f0d0a6a4a35f3d0779e82403374fc08a2d
Crowdbotics <team@crowdbotics.com>
2023-09-21 03:04:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-brook-43485",
+  url: "https://flat-brook-43485.botics.co",
+  api: "https://flat-brook-43485.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
